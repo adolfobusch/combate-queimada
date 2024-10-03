afa7cb66df2abaed44f4eeb494e7a2e158376b51
adolfobusch <138783725+adolfobusch@users.noreply.github.com>
2024-10-03 09:15:52 -0300

    terminado

--- script.js
+++ script.js
@@ -6,39 +6,36 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "A melhor solução para evitar queimadas é a prevenção. Conscientizar a população pela cobrança intensa das instituições de controle e combate a queimadas ilegais é um dever de todos. Conversar com as crianças sobre os perigos que as brincadeiras com o fogo podem provocar também pode ser eficaz para evitar acidentes no futuro ?",
         alternativas: [
-            "Isso é assustador!",
-            "Isso é maravilhoso!"
+            "No dia-a-dia, evitar descartar bitucas de cigarros em áreas de vegetação seca!",
+            "Queimar, jogar lixos próximos a plantações, matas ou canaviais!"
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "O calor é a fuligem tornam o ar capaz de conduzir corrente elétrica entre a rede elétrica é o solo ou entre os fios. Isso provoca o desligamento  da rede e causa a falta de energia. A falta de luz ocorre não só na região do fogo, como também em várias outras. Pode afetar as escalas, as lojas, os hospitais e muitas outras instituições?",
         alternativas: [
-            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-            "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+            "A queimada pode causar a interrupção do fornecimento de energia elétrica pois o calor e as chamas podem danificar os cabos e equipamentos da rede elétrica!",
+            "a queimada proxima da rede elétrica não tem impacto nenhum e a energia elétrica continuará funcionando norm
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "As consequências das queimadas, de modo geral, são prejudiciais, tanto ao meio ambiental quanto á saúde humana. Imagine que uma floresta seja completamente tomada por uma incẽndio.De forma direta, a queimada causará a destruição ambiental de toda a área, perdendo toda a vida selvagem e as riquezas naturais",
         alternativas: [
-            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+            "As queimadas podem causar a destruição de habitats naturais, levando á perda de biodiversidade e ao deslocamento de animais",
+            "As queimadas não tem nenhum efeito no clima, pois não liberam gases poluentes ou afetem a qualidade do ar",
+        ]
+        
+    },
+    {
+        enunciado: "O Instituto Nacional de Pesquisas Espaciais (Inpe) e o Ministério do Meio Ambiente são as instituições responsáveis pelo monitoramento e controle de queimadas no país. Cabe principalmente ao Ministério a elaboração de planos de ação para o combate aos focos de incêndio e o financiamento de equipamento e aviões para o controle",
+        alternativas: [
+            "Implementar politicas de fiscalização rigorosas e campanhas de conscientização.Isso inclui aumentar a presença de agentes ambientais nas áreas de risco e educar a população sobre os impactos das queimadas, incentivando práticas agricolas sustentáveis e alternativas ao uso do fogo",
+            "Ignorar a participação das comunidades locais e impor restrições sem diálogo A falta de envolvimento das populações que vivem nas áreas afetadas podem levar á resistência e desinformação, dificultando a efetividade das medidas de controle"
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: [
-            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-            "Criar uma imagem utilizando um gerador de imagem de IA."
-        ]
-    },
-    {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
-        alternativas: [
-           "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-            "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
-        ]
+        enunciado: "As queimadas são um problema ambiental significativo, afetando a biodiversidade,a qualidade do ar e o clima. Elas ocorrem principalmente na Amazônia, no Cerrado e na Mata Atlântica, frequentemente associadas á expansão agricola e pecuária. O uso do fogo para desmatamento e limpeza de terrenos é uma pratica comum, mas resulta em graves consequências, como a perda de habitat, emissão de gases do efeito estufa e danos á saúde pública",
     },
 ];
 
@@ -48,6 +45,39 @@ let perguntaAtual;
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();{
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
